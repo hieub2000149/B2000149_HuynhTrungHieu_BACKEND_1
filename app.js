8fb2cb9f60b673ecb73789b55050242a0f47e851
hieub2000149 <hieub2000149@student.ctu.edu.vn>
2024-09-19 16:13:16 +0700

    Cai dat va xu ly loi

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const contactsRouter = require("./app/routes/contact.route")
-//const ApiError = require("./app/api-error")
+const ApiError = require("./app/api-error")
 //const port = 3000
 const app = express()
 app.use(cors())
@@ -10,13 +10,13 @@ app.get("/",(req,res) => {
     res.json({message: "Welcome to contact book application."})
 })
 app.use("/api/contacts", contactsRouter)
-//app.use((req, res, next) => {
-//    return next(new ApiError(404, "Resource not found"))
-//})
-//app.use((err, req, res, next) => {
-//    return res.status(err.statusCode || 500).json({
-//        message: err.message || "Internal Server Error",
-//    })
-//})
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"))
+})
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    })
+})
 
 module.exports = app
